71cc935d0d9ec380bdab7911452618fcae04874d
lennart080 <136999768+lennart080@users.noreply.github.com>
2023-11-13 21:57:07 +0100

    new permutation generation

--- SimulationData.java
+++ SimulationData.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class SimulationData {
   private PerlinNoise noise;
   private int[][] board; 
@@ -7,6 +11,7 @@ public class SimulationData {
   public SimulationData() {
     seed = 23;
     noise = new PerlinNoise(seed);
+    this.Write();
     //newBoard(100, 70);
   }
   
@@ -65,18 +70,48 @@ public class SimulationData {
     return lightIntensityAtTime;
   }
 
+    public void Write() {
+      for (int x = 0; x < noise.getpermut().length; x++) {
+        System.out.println(x + ": " + noise.getpermut()[x]);
+      }
+    }
+
+
+
   //klasse und methoden zur erstellung des 1d perlin noise
   
   private static class PerlinNoise {
     private static final int TABLE_SIZE = 2048;
-    private static final int[] permutation = new int[2*TABLE_SIZE];
+    private static final int[] permutation = new int[TABLE_SIZE];
 
-    public PerlinNoise(int pSeed) {                                 //erstellen eines prelin musters am anfag der simulation 
+    private int seed;
+
+    private int[] originalTable = new int[TABLE_SIZE]; 
+    private int[] permutationIndices = new int[TABLE_SIZE];
+    private boolean[] used = new boolean[TABLE_SIZE];
+
+    public PerlinNoise(int pSeed) {    
+      seed = pSeed;                             //erstellen eines prelin musters am anfag der simulation 
       for (int i = 0; i < TABLE_SIZE; i++) {
-        permutation[i] = permutation[i + TABLE_SIZE] = (i + pSeed) & (TABLE_SIZE - 1); 
+        int index;
+        do {
+          index = (int) (XorShiftNext() % TABLE_SIZE);
+        } while (used[index]);
+        permutationIndices[i] = index;
+        used[index] = true;
+      }
+      for (int i = 0; i < TABLE_SIZE; i++) {
+        permutation[i] = originalTable[permutationIndices[i]];
       }
     }
 
+    public long XorShiftNext() {
+        seed ^= (seed << 21);
+        seed ^= (seed >>> 35);
+        seed ^= (seed << 4);
+        return seed;
+    }
+  
     private double fade(double t) {
       return t * t * t * (t * (t* 6 -15) +10);
     }
@@ -101,6 +136,10 @@ public class SimulationData {
       System.out.println("c :" + c + " xf: " + xf + " u: "+ u);
       System.out.println("lerp: " + (lerp(u, grad(permutation[c], xf), grad(permutation[c + 1], xf - 1)) * 2));
       return lerp(u, grad(permutation[c], xf), grad(permutation[c + 1], xf - 1)) * 2; 
+    }
+
+    public int[] getpermut() {
+      return permutation;
     }
   }
 }
