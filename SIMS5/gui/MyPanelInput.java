92b2995a08ac92b50833172874d255465b54b522
denisg05 <150614497+denisg05@users.noreply.github.com>
2024-01-10 00:09:34 +0100

    Warum geht es nicht lenny
    Auf verschiedene Arten versucht nix geschafft außer dass unser balckscreen duchsichtig wird

--- SIMS5/gui/MyPanelInput.java
+++ SIMS5/gui/MyPanelInput.java
@@ -2,11 +2,23 @@ package SIMS5.gui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 import javax.swing.JPanel;
 public class MyPanelInput extends JPanel {
 
   public MyPanelInput() {
     this.setBackground(Color.MAGENTA);
+    /*File pathtoFile = new File("Grafik/black-screen.png"); //HAB AUFGEBEN BRAUCHE HILFE
+    try {                                                    //HAB AUFGEBEN BRAUCHE HILFE
+      Image backround = ImageIO.read(pathtoFile);            //HAB AUFGEBEN BRAUCHE HILFE
+    } catch (IOException e) {                                //HAB AUFGEBEN BRAUCHE HILFE
+      e.printStackTrace();
+    }*/
     repaint(); 
   }
 
@@ -16,6 +28,7 @@ public class MyPanelInput extends JPanel {
     Graphics2D g2D = (Graphics2D) g;
     g2D.setPaint(Color.WHITE);
     g2D.drawString("Input-Panel", 20, 40);
+   // g2D.drawImage(backround, 0, 0, null);
   }
 
   public void myUpdate() {                 //aktualisiren der daten
